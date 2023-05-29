97c0a2e38ef9f233ee498c2a913011a4bc9ee0bb
sudirboyy <sudirboyy@gmail.com>
2023-05-29 14:54:57 +0700

    2

--- paradigmaoop2_178/paradigmaoop2_178.cpp
+++ paradigmaoop2_178/paradigmaoop2_178.cpp
@@ -2,4 +2,16 @@
 #include <string>
 using namespace std;
 
+class orang {
+public:
+	string nama;
+
+	orang(string pNama) :
+		nama(pNama) {
+		cout << "orang dibuat\n" << endl;
+	}
+	~orang() {
+		cout << "orang dihapus\n" << endl;
+	}
+};
 
