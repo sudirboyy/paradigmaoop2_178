55ec8077c7f12acaa1854cc58b13e9bb6a5011ae
sudirboyy <sudirboyy@gmail.com>
2023-05-29 15:54:45 +0700

    17

---
+++ polimorpi/polimorpi.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+class seseorang {
+	virtual void pesan() = 0;
+	//virtual void pesan(){
+	//cout<<"Pesan dari seseorang"<<endl;
+	//}
+};
+
+class joko :public seseorang {
+public:
+	void pesan() {
+		cout << "Pesan dari joko" << endl;
+	}
+};
+
+class lia :public seseorang {
+public:
+	void pesan() {
+		cout << "Pesan dari lia" << endl;
+	}
+};
+
+int main() {
+	seseorang* obyek;
+	joko a;
+	lia b;
+
+	obyek = &a;
+	obyek->
+}
