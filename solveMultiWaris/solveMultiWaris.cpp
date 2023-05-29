0dad034cd7e5367fdf11f7a1c3adb183faa34182
sudirboyy <sudirboyy@gmail.com>
2023-05-29 15:39:22 +0700

    14

--- solveMultiWaris/solveMultiWaris.cpp
+++ solveMultiWaris/solveMultiWaris.cpp
@@ -21,3 +21,14 @@ public:
 		cout << "pekerja dibuat\n" << endl;
 	}
 };
+
+class pelajar : virtual public orang {
+public:
+
+	pelajar(int pUmur) :
+		orang(pUmur)
+	{
+		cout << "pelajar dibuat\n" << endl;
+	}
+};
+
